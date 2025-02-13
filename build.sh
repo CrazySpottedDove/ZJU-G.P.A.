f9a38d59d1d8f39aae9ca43635b6aa3cd6ba6c1c
CrazySpottedDove <3230104178@zju.edu.cn>
2025-02-13 10:55:13 +0800

    edit compile script

--- build.sh
+++ build.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+cd ./main || exit
 compile_time=11
 
 for i in $(seq 1 $compile_time)
 do
-    xelatex -output-directory=build -interaction=nonstopmode \
-  -file-line-error ./main/main.tex
+    xelatex -output-directory=./build -interaction=nonstopmode \
+  -file-line-error ./main.tex
 done
-mv ./main/build/main.pdf ./main/build/ZJU-GPA.pdf
+mv ./build/main.pdf ./build/ZJU-GPA.pdf
 
 for i in $(seq 1 $compile_time)
 do
-    xelatex -output-directory=build -interaction=nonstopmode \
-  -file-line-error "\PassOptionsToClass{dark}{Physics_H_Notes}\input{./main/main.tex}"
+    xelatex -output-directory=./build -interaction=nonstopmode \
+  -file-line-error "\PassOptionsToClass{dark}{Physics_H_Notes}\input{./main.tex}"
 done
-mv ./main/build/main.pdf ./main/build/ZJU-GPA-dark.pdf
+mv ./build/main.pdf ./build/ZJU-GPA-dark.pdf
 
 for i in $(seq 1 $compile_time)
 do
-    xelatex -output-directory=build -interaction=nonstopmode \
-  -file-line-error "\PassOptionsToClass{dark,stable}{Physics_H_Notes}\input{./main/main.tex}"
+    xelatex -output-directory=./build -interaction=nonstopmode \
+  -file-line-error "\PassOptionsToClass{dark,stable}{Physics_H_Notes}\input{./main.tex}"
 done
-mv ./main/build/main.pdf ./main/build/ZJU-GPA-dark-stable.pdf
+mv ./build/main.pdf ./build/ZJU-GPA-dark-stable.pdf
 
 for i in $(seq 1 $compile_time)
 do
-    xelatex -output-directory=build -interaction=nonstopmode \
-  -file-line-error "\PassOptionsToClass{stable}{Physics_H_Notes}\input{./main/main.tex}"
+    xelatex -output-directory=./build -interaction=nonstopmode \
+  -file-line-error "\PassOptionsToClass{stable}{Physics_H_Notes}\input{./main.tex}"
 done
-mv ./main/build/main.pdf ./main/build/ZJU-GPA-stable.pdf
+mv ./build/main.pdf ./build/ZJU-GPA-stable.pdf
